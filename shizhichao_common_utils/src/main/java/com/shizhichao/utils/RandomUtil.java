48041f0ec78ab6d76fbb4da94d1cedfe2eb11deb
szc-0815 <shizhichao5@outlook.com>
2019-12-08 18:44:46 +0800

    修改工具类

---
+++ shizhichao_common_utils/src/main/java/com/shizhichao/utils/RandomUtil.java
@@ -0,0 +1,39 @@
+package com.shizhichao.utils;
+
+import java.util.Random;
+
+//随机工具类
+public class RandomUtil {
+
+	/**
+	 * @Title: random   
+	 * @Description: 获得最小数和最大数之间的随机数   
+	 * @param: @param min
+	 * @param: @param max
+	 * @param: @return      
+	 * @return: int      
+	 * @throws
+	 */
+	public static int random(int min,int max) {
+		Random random = new Random();
+		return min+random.nextInt(max-min+1);
+	}
+	/**
+	 * @Title: random   
+	 * @Description: 获得最小数和最大数之间的多个随机数
+	 * @param: @param min
+	 * @param: @param max
+	 * @param: @param num
+	 * @param: @return      
+	 * @return: int[]      
+	 * @throws
+	 */
+	public static int[] random(int min,int max,int num) {
+		int[] intArray = new int[num];
+		for (int i = 0; i < num; i++) {
+			intArray[i] = random(min, max);
+		}
+		return intArray;
+	}
+
+}
