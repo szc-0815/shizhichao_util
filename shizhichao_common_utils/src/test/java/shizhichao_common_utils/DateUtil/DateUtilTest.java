e7bb68fe592a2a8beb3ac4ceead939682a4b80ca
szc-0815 <shizhichao5@outlook.com>
2019-12-08 18:47:15 +0800

    测试类

---
+++ shizhichao_common_utils/src/test/java/shizhichao_common_utils/DateUtil/DateUtilTest.java
@@ -0,0 +1,16 @@
+package shizhichao_common_utils.DateUtil;
+
+import org.junit.Test;
+
+import com.shizhichao.utils.DateUtil;
+
+public class DateUtilTest {
+
+	@Test
+	public void test() {
+		System.out.println(DateUtil.getAge("2018-10-24"));
+	}
+	
+	
+	
+}
