25adbe2d2d29f5ddd71693046086223309bf4670
szc-0815 <shizhichao5@outlook.com>
2020-01-11 16:16:26 +0800

    完善

--- shizhichao_common_utils/src/main/java/com/shizhichao/utils/FileUtil.java
+++ shizhichao_common_utils/src/main/java/com/shizhichao/utils/FileUtil.java
@@ -2,9 +2,11 @@ package com.shizhichao.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,27 +56,97 @@ public class FileUtil {
 	 */
 	
 	
-	public static String readTextFileByLine(String pathname) {
-		BufferedReader br = null;
-		StringBuffer sb = new StringBuffer();
-		String str="";
-		try {
-			br = new BufferedReader(new FileReader(new File(pathname)));
-		while((str=br.readLine())!=null) {
-			sb.append(str);
-			sb.append("\r\n");
+//	public static String readTextFileByLine(String pathname) {
+//		BufferedReader br = null;
+//		StringBuffer sb = new StringBuffer();
+//		String str="";
+//		try {
+//			br = new BufferedReader(new FileReader(new File(pathname)));
+//		while((str=br.readLine())!=null) {
+//			sb.append(str);
+//			sb.append("\r\n");
+//		}
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//			return null;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return null;
+//		}finally {
+//			StreamUtil.closeAll(br);
+//		}
+//		return sb.toString();
+//	}
+	
+	
+	
+	/*
+	 * 方法1：批量关闭流，参数能传无限个。(3分) 可变参数 例如传入FileInputStream对象、JDBC中Connection对象都可以关闭。
+	 */
+	public static void closeAll(AutoCloseable... autoCloseable) {
+
+		if (null != autoCloseable && autoCloseable.length > 0) {
+			for (AutoCloseable autoCloseable2 : autoCloseable) {
+				try {
+					autoCloseable2.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;
+
+	}
+	/*
+	 * 方法2：传入一个文本文件对象，默认为UTF-8编码，返回该文件内容(3分)，要求方法内部调用上面第1个方法关闭流(3分)
+	 */
+	public static String readTextFile(InputStream src) {
+
+		byte[] b = new byte[1024];
+		int x = 0;
+		String str = "";
+		try {
+			while ((x = src.read(b)) != -1) {
+				str += new String(b, 0, x, "utf-8");
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
-			return null;
-		}finally {
-			StreamUtil.closeAll(br);
+		} finally {// 关流
+			closeAll(src);
 		}
-		return sb.toString();
+		return str;
 	}
+
+	/*
+	 * 方法3：传入文本文件对象，返回该文件内容(3分)，并且要求内部调用上面第2个方法(5分)。* 这是典型的方法重载，记住了吗？少年…
+	 */
+	public static String readTextFileByLine(File txtFile) throws FileNotFoundException {
+		return readTextFile(new FileInputStream(txtFile));
+	}
+	public static String readTextFileByLine(String pathname) throws FileNotFoundException {
+		return readTextFileByLine(new File(pathname));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/**
 	 * @Title: readTextFileOfList   
 	 * @Description: 按行读取文件内容到list集合   
